3712f6656c6820c01179aa0fe62dd9dfa1f9a426
Priyansu Sikdar <priyansusikdar36@gmail.com>
2026-03-26 22:22:09 +0530

    Delete frontend/js/chatbot.js

--- frontend/js/chatbot.js
+++
@@ -1,5 +0,0 @@
-function sendMsg(){
-    let msg = document.getElementById("msg").value;
-    document.getElementById("chat").innerHTML += `<p>You: ${msg}</p>`;
-    document.getElementById("chat").innerHTML += `<p>Bot: Stay hydrated!</p>`;
-}
